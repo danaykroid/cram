890f1f656a38f66eb49b063020e5bfa690bf7ea4
Danaykroid <freecsdon30@gmail.com>
2026-07-30 02:04:06 +0530

    feat: add quill logging lib

--- cmd/main.cpp
+++ cmd/main.cpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
 #include <cram/cram.hpp>
+#include<quill/SimpleSetup.h>
+#include<quill/LogMacros.h>
 using namespace std;
 
 
-int main(){
-	cout<<"test"<<endl;
+int main(int argc,char * argv[]){
+	auto* logger = quill::simple_logger();
+	LOG_INFO(logger, "hello free software enthusiasts {}", "this is free btw");
 }
